326be282e88b157347e9adf5b13205b70cd9bfcc
james <0nlyoung7@gmail.com>
2014-10-18 23:40:29 +0900

    Add upload test

--- test/test.js
+++ test/test.js
@@ -6,209 +6,272 @@ var xpush, xpush1, xpush2;
 var USERS = ['user1','user2','user3','user4'];
 var PASS = ['win1234','win1234','win1234','win1234'];
 
+var appId = 'xpush-client';
+
+// Clear mongoDB
+var mongo = require('../node_modules/mongodb');
+
+var host = 'localhost';
+var client;
+
 describe('XPUSH Client Test', function(){
 
-	before(function(){
+  before(function(){
 
-	  xpush = new XPush('http://demo.stalk.io:8000', 'xpush-client' );
+    xpush = new XPush('http://'+host+':8000', appId );
 
-	  xpush1 = new XPush('http://demo.stalk.io:8000', 'xpush-client' );
+    xpush1 = new XPush('http://'+host+':8000', appId );
 
-	  // Login lately
-	  xpush2 = new XPush('http://demo.stalk.io:8000', 'xpush-client' );
+    // Login lately
+    xpush2 = new XPush('http://'+host+':8000', appId );
 
-	});
+    var server = new mongo.Server(host, 27017, {auto_reconnect: true});
+    var db = new mongo.Db('xpush', server);
+
+    db.open(function(err, db) {
+      if(!err) {
+        client = db;
+      }
+    });
+
+  });
+
+
+  after(function(){
+
+
+    client.collection('users', function( err, c ) {
+      c.remove({'A':appId}, {safe:true}, function(err, result) {
+        if (err) {
+          console.log('An error has occurred - ' + err);
+        } else {
+          assert.equal(err, null); 
+        }
+      });
+    });
+
+    client.collection('channels', function( err, c ) {
+      c.remove({'A':appId}, {safe:true}, function(err, result) {
+        if (err) {
+          console.log('An error has occurred - ' + err);
+        } else {
+          assert.equal(err, null);  
+        }
+      });
+    });
+
+    client.collection('unreadmessages', function( err, c ) {
+      c.remove({'A':appId}, {safe:true}, function(err, result) {
+        if (err) {
+          console.log('An error has occurred - ' + err);
+        } else {
+          assert.equal(err, null); 
+        }
+      });
+    });
+
+  });
 
   this.timeout(10000);
 
-	describe("#Signup Users", function() {
-		console.log('\n\n- - - - - - - - -');
+  describe("#Signup Users", function() {
 
+    // /user/register
     it("signup user1", function(done) {
-			xpush.signup(USERS[0],PASS[0], function(err, data){
-				if( typeof data == 'object'&& data.status ){
-					assert.equal(data.status, "ok");
-				} else {
-					assert.equal(data, "ERR-USER_EXIST");	
-				}
-				done();
-			});
+      xpush.signup(USERS[0],PASS[0], function(err, data){
+        if( typeof data == 'object'&& data.status ){
+          assert.equal(data.status, "ok");
+        } else {
+          assert.equal(data, "ERR-USER_EXIST"); 
+        }
+        done();
+      });
     });
 
     it("signup user2", function(done) {
-			xpush.signup(USERS[1],PASS[1], function(err, data){
-				if( typeof data == 'object'&& data.status ){
-					assert.equal(data.status, "ok");
-				} else {
-					assert.equal(data, "ERR-USER_EXIST");	
-				}
-				done();
-			});
+      xpush.signup(USERS[1],PASS[1], function(err, data){
+        if( typeof data == 'object'&& data.status ){
+          assert.equal(data.status, "ok");
+        } else {
+          assert.equal(data, "ERR-USER_EXIST"); 
+        }
+        done();
+      });
     });
 
     it("signup user3", function(done) {
-			xpush.signup(USERS[2],PASS[2], function(err, data){
-				if( typeof data == 'object'&& data.status ){
-					assert.equal(data.status, "ok");
-				} else {
-					assert.equal(data, "ERR-USER_EXIST");	
-				}
-				done();
-			});
+      xpush.signup(USERS[2],PASS[2], function(err, data){
+        if( typeof data == 'object'&& data.status ){
+          assert.equal(data.status, "ok");
+        } else {
+          assert.equal(data, "ERR-USER_EXIST"); 
+        }
+        done();
+      });
     });
 
-	});
+  });
 
-	describe("#Login Users", function() {
-		console.log('\n\n- - - - - - - - -');
+  describe("#Login Users", function() {
 
+    // auth, node
     it("login user1", function(done) {
-    	xpush.login(USERS[0],PASS[0],function(err, data){
-    		assert.equal( USERS[0], data.user.U );
-    		done();
-    	});
+      xpush.login(USERS[0],PASS[0],function(err, data){
+        assert.equal( USERS[0], data.user.U );
+        done();
+      });
     });
 
     it("login user2", function(done) {
-    	xpush1.login(USERS[1],PASS[1],function(err, data){
-    		assert.equal( USERS[1],  data.user.U );
-    		done();
-    	});
+      xpush1.login(USERS[1],PASS[1],function(err, data){
+        assert.equal( USERS[1],  data.user.U );
+        done();
+      });
     });
 
-	});
+  });
 
-	describe("#Users Test", function() {
-		console.log('\n\n- - - - - - - - -');
+  describe("#Users Test", function() {
 
+    // user-query
     it("Query user", function(done) {
-    	var param = {query : {'U':'user1'}, column: { U: 1, DT: 1, _id: 0 } };
-			xpush.queryUser( param, function( err, userArray, count ){
-    		done();
-    	});
+      var param = {query : {'U':'user1'}, column: { U: 1, DT: 1, _id: 0 } };
+      xpush.queryUser( param, function( err, userArray, count ){
+        done();
+      });
     });
 
-	});
+  });
 
-	var randomChannel;
-	describe("#Channel Create Test", function() {
-		console.log('\n\n- - - - - - - - -');
+  var randomChannel;
+  describe("#Channel Create Test", function() {
 
-		it("create random channel without data", function(done) {
+    // channel-create
+    it("create random channel without data", function(done) {
 
-	    xpush.createChannel(['user1'], function(err, data){
-	    	randomChannel = data;
-    		assert.equal(err, undefined);
-    		done();
-	    });
+      xpush.createChannel(['user1'], function(err, data){
+        randomChannel = data;
+        assert.equal(err, undefined);
+        done();
+      });
 
-	  });
+    });
 
-		it("create custom channel without data", function(done) {
+    it("create custom channel without data", function(done) {
 
-	    xpush.createChannel(['user1', 'user2'], 'channel01', function(err, data){
-    		assert.equal( data, 'channel01' );
-    		done();
-	    });
+      xpush.createChannel(['user1', 'user2'], 'channel01', function(err, data){
+        assert.equal( data, 'channel01' );
+        done();
+      });
 
-	  });
+    });
 
-		it("create a channel with data", function(done) {
+    it("create a channel with data", function(done) {
 
-	    xpush.createChannel(['user1', 'user2'], 'channel02', {'NM':'channelName02'}, function(err, data){
-    		assert.equal( USERS.U, data.U );
-    		done();
-	    });
+      xpush.createChannel(['user1', 'user2'], 'channel02', {'NM':'channelName02'}, function(err, data){
+        assert.equal( USERS.U, data.U );
+        done();
+      });
 
-	  });
+    });
 
-	});
+  });
 
-	describe("#Channel Manage Test", function() {
-		console.log('\n\n- - - - - - - - -');
+  describe("#Channel Manage Test", function() {
 
-		it("Update Channel", function(done) {
+    // channe-update
+    it("Update Channel", function(done) {
 
-    	xpush.updateChannel( 'channel02', { $set:{'DT':{'NM':'updateChannelName03'}}}, function(err, result){
-    		assert.equal( 'updateChannelName03', result.DT.NM );
-    		done()
-     	});
+      xpush.updateChannel( 'channel02', { $set:{'DT':{'NM':'updateChannelName03'}}}, function(err, result){
+        assert.equal( 'updateChannelName03', result.DT.NM );
+        done()
+      });
 
-		});
+    });
 
-		it("Exit Channel", function(done) {
+    // channel-exit
+    it("Exit Channel", function(done) {
 
-			xpush.exitChannel(randomChannel, function(err, data){
-				assert.equal(err, undefined);
-				done();
-			});
+      xpush.exitChannel(randomChannel, function(err, data){
+        assert.equal(err, undefined);
+        done();
+      });
 
-		});
+    });
 
-		it("Get Channels", function(done) {
+    //channel-get
+    it("Get Channels", function(done) {
 
-			xpush.getChannels(function(err, datas){
-				assert.equal( 2, datas.length );
-				done();
-			});
+      xpush.getChannels(function(err, datas){
+        assert.equal( 2, datas.length );
+        done();
+      });
 
-		});
+    });
 
-		it("Join Channel", function(done) {
+    //channel-join
+    it("Join Channel", function(done) {
 
-    	xpush.joinChannel( 'channel01', {'U':['user3']}, function(result){
-	    	xpush.getChannelData( 'channel01', function(err, result){
-					assert.equal(3, result.US.length);
-					done();
-				});
-			});
+      xpush.joinChannel( 'channel01', {'U':['user3']}, function(result){
 
-		});
+        // channel-get
+        xpush.getChannelData( 'channel01', function(err, result){
+          assert.equal(3, result.US.length);
+          done();
+        });
+      });
 
-	});
+    });
 
-	describe("#Group Manage Test", function() {
-		console.log('\n\n- - - - - - - - -');
+  });
 
-		it("addUserToGroup", function(done) {
+  describe("#Group Manage Test", function() {
 
-    	xpush.addUserToGroup( 'user1', ['user2','user3'], function( err, result ){
-    		xpush.getGroupUsers( 'user1', function( err, result ){
-	    		assert.equal( 2,  result.length );
-	    		done();
-    		});
-     	});
+    it("addUserToGroup", function(done) {
 
-		});
+      //emit group-add
+      xpush.addUserToGroup( 'user1', ['user2','user3'], function( err, result ){
 
-		it("removeUserFromGroup", function(done) {
+        // Get group user by emitting group-list
+        xpush.getGroupUsers( 'user1', function( err, result ){
+          assert.equal( 2,  result.length );
+          done();
+        });
+      });
 
-    	xpush.removeUserFromGroup( 'user1', 'user2', function( err, result ){
-    		xpush.getGroupUsers( 'user1', function( err, result ){
-	    		assert.equal( 1,  result.length );
-	    		done();
-    		});
-     	});
+    });
 
-		});
+    it("removeUserFromGroup", function(done) {
 
-	});
+      // emit group-remove
+      xpush.removeUserFromGroup( 'user1', 'user2', function( err, result ){
 
-	describe("#Message Test", function() {
-		console.log('\n\n- - - - - - - - -');
+        // Get group user by emitting group-list
+        xpush.getGroupUsers( 'user1', function( err, result ){
+          assert.equal( 1,  result.length );
+          done();
+        });
+      });
 
-		it("send message", function(done) {
+    });
 
-			xpush.on( 'message', function( ch, name, data ){
+  });
+
+  describe("#Message Test", function() {
+
+    it("send message", function(done) {
+
+      xpush.on( 'message', function( ch, name, data ){
         assert.equal(data.MG, 'Hello world');
         checkComplete();
-			});
+      });
 
-			xpush1.on( 'message', function( ch, name, data ){
+      xpush1.on( 'message', function( ch, name, data ){
         assert.equal(data.MG, 'Hello world');
         checkComplete();
-			});
+      });
 
+      //send
       xpush.send( 'channel01', 'message', {'MG':'Hello world'} );
 
       var count = 2;
@@ -219,19 +282,20 @@ describe('XPUSH Client Test', function(){
         }
       };
 
-		});
+    });
 
     it("unread message user3", function(done) {
 
-    	xpush2.on( 'message', function( ch, name, data ){
-				assert.equal(data.MG, 'Hello world');
-				checkComplete();
-    	});
+      xpush2.on( 'message', function( ch, name, data ){
+        assert.equal(data.MG, 'Hello world');
+        checkComplete();
+      });
 
-    	xpush2.login(USERS[2],PASS[2],function(err, data){
-    		assert.equal( USERS[2],  data.user.U );
-    		checkComplete();
-    	});
+      // retrieve unread message when login
+      xpush2.login(USERS[2],PASS[2],function(err, data){
+        assert.equal( USERS[2],  data.user.U );
+        checkComplete();
+      });
 
       var count = 2;
       var checkComplete = function(){       
@@ -243,5 +307,34 @@ describe('XPUSH Client Test', function(){
       
     });
 
-	});
+  });
+
+  describe("#Upload file", function() {
+
+    var uploadResult;
+
+    it("Upload file with rest api", function(done) {
+      var options = { name: 'file.txt' };
+
+      xpush.uploadFile( 'channel01', 'file.txt', options, 
+        function( prg ){
+          console.log( prg );
+        },
+        function( data ){
+          uploadResult = JSON.parse(data).result;
+          assert.equal('ok', JSON.parse(data).status); 
+          done();
+        }
+      );
+
+    });
+
+    it("Download file with rest api", function(done) {
+      console.log( uploadResult );
+      var url = xpush.getFileUrl( uploadResult.channel, uploadResult.name );
+      ssert.equal('ok', JSON.parse(data).status);
+      done();
+    });
+  });
+
 });
